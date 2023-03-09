f2c625159974769db58b23ac9eaf4b99dd8f8fbb
BOMBARED12 <wildter1995@gmail.com>
2023-03-09 14:44:27 -0600

    MODULE 6

--- src/App.js
+++ src/App.js
@@ -1,35 +1,22 @@
 import "./App.css";
 import { userState, useState } from "react";
 import { User } from "./User";
+import { Task } from "./Task";
+import { Text } from "./Text";
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
-  const [newTask, setNewTask] = useState("");
-
-  const handleChange = (event) => {
-    setNewTask(event.target.value);
-  };
-
-  const addTask = () => {
-    const newTodoList = [...todoList, newTask];
-    setTodoList(newTodoList);
-  };
+  const [showText, setShowText] = useState(false);
 
   return (
     <div className="App">
-      <div className="addTask"></div>
-      <input onChange={handleChange} />
-      <button onClick={addTask}>Add Task</button>
-      <div className="list">
-        {todoList.map((task) => {
-          return (
-            <div>
-              <h1>{task}</h1>
-              <button>x</button>
-            </div>
-          );
-        })}
-      </div>
+      <button
+        onClick={() => {
+          setShowText(!showText);
+        }}
+      >
+        Show Text
+      </button>
+      {showText && <Text />}
     </div>
   );
 }
