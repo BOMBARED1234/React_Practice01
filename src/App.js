5443dd1d4b5f1757a950b05f4282dc9c917fba49
BOMBARED12 <wildter1995@gmail.com>
2023-03-09 19:28:02 -0600

    Module 8

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import "./App.css";
-import { userState, useState } from "react";
+import { useEffect, useState } from "react";
+import Axios from "axios";
 import { User } from "./User";
 import { Task } from "./Task";
 import { Text } from "./Text";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Menu } from "./pages/Menu";
+import { Contact } from "./pages/Contact";
+import { Navbar } from "./Navbar";
 
 function App() {
-  const [showText, setShowText] = useState(false);
-
   return (
     <div className="App">
-      <button
-        onClick={() => {
-          setShowText(!showText);
-        }}
-      >
-        Show Text
-      </button>
-      {showText && <Text />}
+      <Router>
+        <div>
+          <Navbar />
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/contact" element={<Contact Page />} />
+          <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
